672e6c7196d5fd76e93810305edbe52aaabfc8bc
Fadi Suleiman <fadi.suleiman@iteratec.com>
2019-10-08 14:47:09 +0200

    Use bootstrap

--- Code.js
+++ Code.js
@@ -1,10 +1,12 @@
-let carousel = document.getElementById("carousel");
+const carousel = document.getElementsByClassName("carousel-inner")[0];
 
-for (let i = 1; i <= 12; i++) {
-	carousel.innerHTML += "<img src='img/ (" + i + ").jpeg'>";
-}	
-
-doScroll = (direction) => {
-	let width = carousel.clientWidth;
-	carousel.scrollLeft += direction === 'left' ? width : -width;
-};
+let carousel_inner = "";
+for (let i = 0; i < 4; i++) {
+	const active = i === 0 ? " active" : "";
+	carousel_inner += "<div class='item" + active + "'><div>";
+    for (let j = 0; j < 3; j++) {
+        carousel_inner += "<img src='img/(" + (i*3 + j + 1) + ").jpeg'>";
+    }
+    carousel_inner += "</div></div>";
+}
+carousel.innerHTML = carousel_inner;
