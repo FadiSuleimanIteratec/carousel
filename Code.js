c02952a57e8118c4dc49968e66c474808f00d815
Fadi Suleiman <fadi.suleiman@iteratec.com>
2019-10-08 10:49:33 +0200

    Carousel

---
+++ Code.js
@@ -0,0 +1,10 @@
+let carousel = document.getElementById("carousel");
+
+for (let i = 1; i <= 12; i++) {
+	carousel.innerHTML += "<img src='img/ (" + i + ").jpeg'>";
+}	
+
+doScroll = (direction) => {
+	let width = carousel.clientWidth;
+	carousel.scrollLeft += direction === 'left' ? width : -width;
+}
